b7fcdc479395163f2c04df880333e229e3fb9cc3
Karen White <karenwhite@karens-mbp-4.attlocal.net>
2020-06-11 14:51:29 -0700

    first commit

--- actions.py
+++ actions.py
@@ -1,8 +1,43 @@
 from typing import Any, Text, Dict, List, Union
+from dotenv import load_dotenv
+from urllib.parse import urljoin
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction
+
+import requests
+import json
+import os
+
+airtable_api_key=os.getenv("AIRTABLE_API_KEY")
+base_id=os.getenv("BASE_ID")
+table_name=os.getenv("TABLE_NAME")
+
+def create_health_log(confirm_exercise, exercise, sleep, diet, stress, goal):
+    request_url=urljoin("https://api.airtable.com/v0", base_id, table_name)
+
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": "Bearer {}'.format(airtable_api_key)'",
+    }  
+    data = {
+  "fields": {
+    "Exercised?": confirm_exercise,
+    "Type of exercise": exercise,
+    "Amount of sleep": sleep,
+    "Stress": stress,
+    "Diet": diet,
+    "Goal": goal,
+  }
+}
+    response = requests.post(
+        request_url, headers=headers, data=json.dumps(data)
+    )
+    return response
+
+    print(response.status_code)
 
 class HealthForm(FormAction):
 
@@ -52,6 +87,22 @@ class HealthForm(FormAction):
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) -> List[Dict]:
+
+        confirm_exercise = tracker.get_slot("confirm_exercise")
+        exercise = tracker.get_slot("exercise")
+        sleep = tracker.get_slot("sleep")
+        stress = tracker.get_slot("stress")
+        diet = tracker.get_slot("diet")
+        goal = tracker.get_slot("goal")
+
+        response = create_health_log(
+                confirm_exercise=confirm_exercise,
+                exercise=exercise,
+                sleep=sleep,
+                stress=stress,
+                diet=diet,
+                goal=goal
+            )
 
         dispatcher.utter_message("Thanks, great job!")
         return []
