a81c837004a5bfd9c8c4e9effd1a419805c47ec5
Karen White <karenwhite@karens-mbp-4.attlocal.net>
2020-11-17 14:12:29 -0800

    converted happy paths to 2.0

--- actions.py
+++ actions.py
@@ -1,9 +1,9 @@
-from typing import Any, Text, Dict, List, Union
+from typing import Any, Text, Dict, List, Union, Optional
 from dotenv import load_dotenv
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.forms import FormAction
+from rasa_sdk.forms import FormValidationAction
 
 import requests
 import json
@@ -44,49 +44,26 @@ def create_health_log(confirm_exercise, exercise, sleep, diet, stress, goal):
     return response
     print(response.status_code)
 
-class HealthForm(FormAction):
+class ValidateHealthForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_health_form"
 
-    def name(self):
-        return "health_form"
-
-    @staticmethod
-    def required_slots(tracker):
-
-        if tracker.get_slot('confirm_exercise') == True:
-            return ["confirm_exercise", "exercise", "sleep",
-             "diet", "stress", "goal"]
+    async def validate_confirm_exercise(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        if value:
+            return {"confirm_exercise": True}
         else:
-            return ["confirm_exercise", "sleep",
-             "diet", "stress", "goal"]
+            return {"exercise": "None", "confirm_exercise": False }
 
-    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
-        """A dictionary to map required slots to
-            - an extracted entity
-            - intent: value pairs
-            - a whole message
-            or a list of them, where a first match will be picked"""
-
-        return {
-            "confirm_exercise": [
-                self.from_intent(intent="affirm", value=True),
-                self.from_intent(intent="deny", value=False),
-                self.from_intent(intent="inform", value=True),
-            ],
-            "sleep": [
-                self.from_entity(entity="sleep"),
-                self.from_intent(intent="deny", value="None"),
-            ],
-            "diet": [
-                self.from_text(intent="inform"),
-                self.from_text(intent="affirm"),
-                self.from_text(intent="deny"),
-            ],
-            "goal": [
-                self.from_text(intent="inform"),
-            ],
-        }
-
-    def submit(
+class ActionSubmitResults(Action):
+    def name(self) -> Text:
+        return "action_submit_results"
+    def run(
         self,
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
